ef52e586217a7437d5d8879f548624274f06201f
Legolas <razatr@yandex.ru>
2019-12-04 14:16:57 +0300

    the breakdown into components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Article from './Article'
+import Header from './Header'
+
+function App() {
+    return (
+      <div className="App">
+        <Header />
+        <span>Finaly add sandbox for my first React App</span>
+        <Article />
+      </div>
+    );
+}
+
+export default App;
---
+++ src/components/Article.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Article(){
+    const body = <section>Its a ARCTICLE!</section>
+        return (
+        <div>
+          <h2>My furst app in React JS</h2>
+          {body}
+        </div>
+  )
+}
+
+export default Article;
